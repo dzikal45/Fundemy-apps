50d424e66b79e9de6ade907995fab32bddde9f54
Muhamad Farid Ridho Rambe <muhamadfaridrr@gmail.com>
2021-11-05 18:48:17 +0700

    Connect API

--- frontend/src/pages/becometeacher.js
+++ frontend/src/pages/becometeacher.js
@@ -7,6 +7,9 @@ import { LogBtn, NavLink, RegBtn } from "../elements/loginElement"
 import gambar from '../component/icons/login image2.png'
 import gambar2 from '../component/icons/login image.png'
 import { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router'
+import axios from 'axios'
 // import Navbar from "../component/navbar/navbarputih"
 
 
@@ -19,6 +22,32 @@ const Teacherauth = () => {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('')
     const [name, setName] = useState('')
+
+    let history = useHistory()
+    const { register, handleSubmit } = useForm()
+    const handleRegis = data => {
+        console.log(data)
+        axios
+            .post("http://localhost:5000/api/guru/register", data)
+            .then(() => {
+                history.push("/")
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    const handleLogin = data => {
+        console.log(data)
+        axios
+            .get("http://localhost:5000/api/guru/login", data)
+            .then(() => {
+                history.push("/")
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
     return (
        
@@ -34,18 +63,18 @@ const Teacherauth = () => {
                             <Row>
                                 <Form.Group controlId='username'>
                                     <h6 style={{color:'#501E65'}}>Username</h6>
-                                    <Form.Control style={{paddingRight:"220px"}} placeholder='learnwithfun' value={username} onChange={(e) => setUsername(e.target.value)}></Form.Control>
+                                    <Form.Control style={{paddingRight:"220px"}} placeholder='learnwithfun' {...register("username")}></Form.Control>
                                 </Form.Group>
                             </Row>
 
                             <Row>
                                 <Form.Group controlId='password'>
                                     <h6 style={{color:'#501E65'}}>Password</h6>
-                                    <Form.Control style={{paddingRight:"220px", marginBottom:"40px"}} type='password' placeholder='********' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                                    <Form.Control style={{paddingRight:"220px", marginBottom:"40px"}} type='password' placeholder='********' {...register("password")}></Form.Control>
                                 </Form.Group>
                             </Row>
 
-                            <LogBtn type='submit' variant='primary'>Login</LogBtn>
+                            <LogBtn type='submit' variant='primary' onSubmit={handleSubmit(handleLogin)}>Login</LogBtn>
 
                         </Form>
                     </Col>
@@ -55,30 +84,30 @@ const Teacherauth = () => {
                     <Row>
                         <Form.Group controlId="name">
                             <h6 style={{color:'#501E65'}}>Name</h6>
-                            <Form.Control style={{BorderRight:"20px"}} placeholder="FUNBuddies" value={name} onChange={(e)=>setName(e.target.value)}></Form.Control>
+                            <Form.Control style={{BorderRight:"20px"}} placeholder="FUNBuddies" {...register("name")}></Form.Control>
                         </Form.Group>
 
                         <Form.Group controlId="username">
                             <h6 style={{color:'#501E65'}}>Username</h6>
-                            <Form.Control placeholder="teamwithfun" value={username} onChange={(e)=>setUsername(e.target.value)}></Form.Control>
+                            <Form.Control placeholder="teamwithfun" {...register("username")}></Form.Control>
                         </Form.Group>
                     </Row>
 
                     <Row>
                         <Form.Group controlId='email'>
                             <h6 style={{color:'#501E65'}}>Email</h6>
-                            <Form.Control style={{paddingRight:"220px"}} type='email' placeholder='teamwithfun@gmail.com' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+                            <Form.Control style={{paddingRight:"220px"}} type='email' placeholder='teamwithfun@gmail.com' {...register("email")}></Form.Control>
                         </Form.Group>
                     </Row>
 
                     <Row>
                         <Form.Group controlId='password'>
                             <h6 style={{color:'#501E65'}}>Password</h6>
-                            <Form.Control style={{paddingRight:"220px"}} type='password' placeholder='Min. 8 Character' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                            <Form.Control style={{paddingRight:"220px"}} type='password' placeholder='Min. 8 Character' {...register("password")}></Form.Control>
                         </Form.Group>
                     </Row>
 
-                    <RegBtn  type='submit' variant='primary'>Create Account</RegBtn>
+                    <RegBtn  type='submit' variant='primary' onSubmit={handleSubmit(handleRegis)}>Create Account</RegBtn>
                 </Form>
                     </Col>
                    
--- frontend/src/pages/registerpage.js
+++ frontend/src/pages/registerpage.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Col, Row, Button, Form } from "react-bootstrap";
 import { NavLink, RegBtn } from '../elements/loginElement'
 import gambar from '../component/icons/login image.png'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router';
 
 const Registerpage = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('')
-    const [name, setName] = useState('')
+    let history = useHistory()
+    const { register, handleSubmit } = useForm()
+    const handleRegis = data => {
+        console.log(data)
+        axios
+            .post("http://localhost:5000/api/user/register", data)
+            .then(() => {
+                history.push("/")
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
     return (
         <Row>
             <Col>
@@ -23,30 +35,30 @@ const Registerpage = () => {
                     <Row>
                         <Form.Group controlId="name">
                             <h6 style={{color:'#501E65'}}>Name</h6>
-                            <Form.Control style={{BorderRight:"20px"}} placeholder="FUNBuddies" value={name} onChange={(e)=>setName(e.target.value)}></Form.Control>
+                            <Form.Control style={{BorderRight:"20px"}} placeholder="FUNBuddies" {...register("name")}></Form.Control>
                         </Form.Group>
 
                         <Form.Group controlId="username">
                             <h6 style={{color:'#501E65'}}>Username</h6>
-                            <Form.Control placeholder="teamwithfun" value={username} onChange={(e)=>setUsername(e.target.value)}></Form.Control>
+                            <Form.Control placeholder="teamwithfun" {...register("username")}></Form.Control>
                         </Form.Group>
                     </Row>
 
                     <Row>
                         <Form.Group controlId='email'>
                             <h6 style={{color:'#501E65'}}>Email</h6>
-                            <Form.Control style={{paddingRight:"220px"}} type='email' placeholder='teamwithfun@gmail.com' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+                            <Form.Control style={{paddingRight:"220px"}} type='email' placeholder='teamwithfun@gmail.com' {...register("email")}></Form.Control>
                         </Form.Group>
                     </Row>
 
                     <Row>
                         <Form.Group controlId='password'>
                             <h6 style={{color:'#501E65'}}>Password</h6>
-                            <Form.Control style={{paddingRight:"220px"}} type='password' placeholder='Min. 8 Character' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                            <Form.Control style={{paddingRight:"220px"}} type='password' placeholder='Min. 8 Character' {...register("password")}></Form.Control>
                         </Form.Group>
                     </Row>
 
-                    <RegBtn  type='submit' variant='primary'>Create Account</RegBtn>
+                    <RegBtn  type='submit' variant='primary' onSubmit={handleSubmit(handleRegis)}>Create Account</RegBtn>
                             
                         <Row>
                             Already Have an Account? &nbsp; <NavLink to ="/login" >Log in</NavLink>
