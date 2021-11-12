8d91a00e49d7a38f99367608514db685e1c2cd27
dzikal45 <dzikal45@gmail.com>
2021-11-12 16:55:24 +0700

    enrooll course

--- backend/controller/user/userController.js
+++ backend/controller/user/userController.js
@@ -5,12 +5,19 @@
 const expressAsyncHandler = require('express-async-handler');
 const generateToken = require('../../utils/generateToken');
 const User = require('../../models/userModel');
+const course = require('../../models/courseModel')
 const bcrypt = require('bcryptjs');
 var Crytpojs = require('crypto-js'); 
-const{registerValidation,loginValidation} = require('../../validation');
+const{registerValidation,loginValidation,deleteCourseValidation} = require('../../validation');
+const uuid = require('uuid');
+const db = require('../../connection/db');
+const firebase = require('firebase');
+const { ObjectId } = require('bson');
+require('firebase/firebase-storage'); // must be required for this to 
 
+const storage = firebase.storage().ref();
 
-
+global.XMLHttpRequest = require("xhr2"); // must be used to avoid bug
 // @desc Auth user & get Token
 // @route POST api/users/login
 // @access Public routes 
@@ -103,12 +110,12 @@ exports.registerUser = expressAsyncHandler(async(req,res) => {
     }
 })
 exports.profileUser = expressAsyncHandler(async(req,res)=>{
-    res.send(req.user)
-    const { id} = req.user.id;
+    // res.send(req.user)
+    const  id = req.user.id;
     
 
 
-    const user = await User.findOne({ id })
+    const user = await User.findOne({_id: id })
 
     if(user){
         res.status(201).json({
@@ -129,5 +136,161 @@ exports.profileUser = expressAsyncHandler(async(req,res)=>{
     }
 
 
+})
+
+
+
+exports.uploadPembayaran = expressAsyncHandler(async(req,res)=>{
+    const id = req.user.id;
+    const {total_payment,subscribe}= req.body;
+    // res.send(total_payment);
+    if(!req.file) {
+        res.status(400).send("Error: No files found")
+    }else{
+        try{
+                  // Grab the file
+                  const file = req.file;
+                  // Format the filename
+                  const timestamp = Date.now();
+                  const name = file.originalname.split(".")[0];
+                  const type = file.originalname.split(".")[1];
+                  const fileName = `${name}_${timestamp}.${type}`;
+                  // Step 1. Create reference for file name in cloud storage 
+                  const imageRef = storage.child(`bukti_pembayaran/${req.user.username}/`+fileName);
+                  const metadata = {
+                      contentType: req.file.mimetype
+                  }
+                  
+                  
+                  const user = await User.findOne({_id:id});
+
+                  
+                  // Step 2. Upload the file in the bucket storage
+                  const snapshot = await imageRef.put(file.buffer,metadata);
+                 // setTimeout(snapshot,60000);
+                  // Step 3. Grab the public url
+                  const downloadURL = await snapshot.ref.getDownloadURL();
+            
+         const Pesanan = {
+                    invoiceId: uuid.v4(),
+                    invoiceFile: downloadURL,
+                    invoice_date: Date.now(),
+                    invoice_status: "pending",
+                    total_payment: total_payment,
+                    subscribe: subscribe
+                }
+
+        // res.send(Pesanan);
+        
+        user.pesanan.push(Pesanan);
+        await user.save();
+        
+        res.status(201).json(user.pesanan)
+           
+
+        }catch(err){
+            console.log (err)
+    res.status(400).send(err.message);
+        }
+        
+    }
+})
+
+exports.getPembayaranPending = expressAsyncHandler(async(req,res)=>{
+    const id = req.user.id
+        try{
+
+            const Pembayaran = await User.findOne({_id:id});
+            const arr = Pembayaran.pesanan
+            var newArr = [];
+            for(i=0;i<arr.length;i++){
+                if(arr[i].invoice_status=="pending" ){
+                    newArr.push(arr[i]);
+                }
+            }
+            res.status(200).json(newArr);
+        }catch(e){
+            console.log (e)
+            res.status(400).send(e.message);
+        }
+})
+
+exports.getPembayaran = expressAsyncHandler(async(req,res)=>{
+    const id = req.user.id;
+    
+    const user = await User.findById(id);
+    if(user){
+
+        res.status(200).json(user.pesanan)
+    }else{
+        res.status(400);
+        throw new Error(`user not found`)
+    }
+})
+
+exports.getPembayaranById = expressAsyncHandler(async(req,res)=>{
+    const user_id = req.user.id;
+    const _id = req.params.id
+    try{
+
+        const user = await User.findById(user_id);
+        const arr = user.pesanan;
+        // res.send(arr);
+        const result = arr.find(function(element){
+            return element._id == _id
+        })
+        res.status(200).json(result)
+    }catch(e){
+        console.log (e)
+        res.status(400).send(e.message);
+    }
+})
+
+exports.enrollCourse = expressAsyncHandler(async(req,res)=>{
+    const user_id = req.user.id;
+    const Course_id = req.body.course_id;
+    const{error} = loginValidation(req.body);
+    if(error)res.status(400).send(error.details[0].message);
+    try{
+
+        const user = await User.findById(user_id);
+        const Course = await course.findById(Course_id);
+        const enroll ={
+            
+            
+        }
+        
+        const newCourse = {
+            date_enrollment: Date.now(),
+            course_name:Course.course_name
+        }
+        // res.send(Course)
+
+    
+        user.course_enrollment.push(newCourse);
+      
+        user.course_id.push(Course._id);
+        await user.save();
+        
+        res.status(201).json(user);
+    }catch(e){
+        console.log (e)
+        res.status(400).send(e.message);
+    }
+
+
+})
+exports.getCourseEnroll = expressAsyncHandler(async(req,res)=>{
+    const id = req.user.id;
+    try{
+    
+        const Course = await User.findById(id).populate('course_id');
+        res.status(200).json({Course})
+        console.log('retrieved list of course', Course.length, Course[0]);
+    }catch(err){
+        console.log (err)
+        res.status(400);
+        throw new Error(err);
+    }
 })
 // module.exports = [registerUser,authUser];
